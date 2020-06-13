4c598f1b0df8bf3a5ec2a84f942fc4ac9f6db06e
Ariadna Gutierrez <58239005+arigovea@users.noreply.github.com>
2020-06-13 14:14:58 -0500

    Partial Solution made by Ari

--- src/components/MultiplyComponent.js
+++ src/components/MultiplyComponent.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
 export class MultiplyComponent extends React.Component {
-    render() {
-        return (
-            <div class="card text-center">
-                <div class="card-body">
-                    <h5 class="card-title">value*2</h5>
-                    <p class="card-text">computed value here!!!</p>
-                </div>
-            </div>
-        );
-    }
+	multiplyData(data) {
+		return 2 * data;
+	}
+	render() {
+		const { data } = this.props;
+		//console.clear();
+		//console.log(data);
+		return !data ? (
+			data
+		) : (
+			<div className="card text-center">
+				<div className="card-body">
+					<h5 className="card-title">value*2</h5>
+					<p className="card-text">Computed value here!!!</p>
+					<p className="card-title">{this.multiplyData(data)} </p>
+				</div>
+			</div>
+		);
+	}
 }
